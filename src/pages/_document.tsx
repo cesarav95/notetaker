3b3ea6cf67b19c696618d464c481217e97afe34f
Cesar E. Achahui Vilca <cesarav751323@gmail.com>
2023-04-10 11:29:39 -0500

    feat: add routes and forms"

---
+++ src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+import React from 'react'
+
+const Document = () => {
+  return (
+    <Html data-theme="winter">
+      <Head></Head>
+      <body>
+        <Main></Main>
+        <NextScript></NextScript>
+      </body>
+    </Html>
+
+  )
+}
+
+export default Document
